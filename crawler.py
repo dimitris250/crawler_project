875e352f7d3a7a222ed33c09448da2bfb950e811
dimitris250 <55361593+dimitris250@users.noreply.github.com>
2021-02-03 12:30:51 +0200

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -4,13 +4,13 @@ import urllib.parse
 import json
 from datetime import datetime, timedelta
 import time
-from selenium import webdriver
+import re
 
 
 
 
 
-url='https://www.vrbo.com/el-gr/%CE%B5%CE%BD%CE%BF%CE%B9%CE%BA%CE%B9%CE%AC%CF%83%CE%B5%CE%B9%CF%82-%CE%B5%CE%BE%CE%BF%CF%87%CE%B9%CE%BA%CF%8E%CE%BD-%CE%BA%CE%B1%CF%84%CE%BF%CE%B9%CE%BA%CE%B9%CF%8E%CE%BD/p436144?adultsCount=2&arrival=2021-05-08&departure=2021-05-16'
+url='https://www.vrbo.com/el-gr/%CE%B5%CE%BD%CE%BF%CE%B9%CE%BA%CE%B9%CE%AC%CF%83%CE%B5%CE%B9%CF%82-%CE%B5%CE%BE%CE%BF%CF%87%CE%B9%CE%BA%CF%8E%CE%BD-%CE%BA%CE%B1%CF%84%CE%BF%CE%B9%CE%BA%CE%B9%CF%8E%CE%BD/p436317?adultsCount=2&arrival=2022-04-21&departure=2022-04-28&uni_id=1061960'
 headers={}
 headers['User-Agent']='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 req= urllib.request.Request(url, headers=headers)
@@ -32,13 +32,41 @@ def get_basic_info():
         lists.append(anemities.text)
     
     items['anemities']= lists
-    
-    data.append(items)
 
+    req2 = requests.get(url).text
+
+    search = re.search(r'window\.__INITIAL_STATE__ = ({.*});', req2).group(1)
+
+    data = json.loads(search)
+
+    items['latitude']= data['listingReducer']['geoCode']['latitude']
+    items['longtitude']=data['listingReducer']['geoCode']['longitude']
+
+    availability = (data['listingReducer']['availabilityCalendar']['availability']['unitAvailabilityConfiguration']['availability'])
+
+    date_regex = r"(\d{4}-\d{2}-\d{2})"
+    arrival_date = re.search(r"arrival=" + date_regex, url).group(1) #extracts arrival date from link
+    departure_date = re.search(r"departure=" + date_regex, url).group(1) #extracts departure date from link
+
+    items['arrival-date']=arrival_date
+    items['departure-date']=departure_date
+
+    arrival_day_of_year = datetime.strptime(arrival_date, '%Y-%m-%d')
+    departure_day_of_year =datetime.strptime(departure_date, '%Y-%m-%d')
+
+    arrival_day_of_year = arrival_day_of_year.timetuple().tm_yday #converts dates to the number of the day in the year
+    departure_day_of_year= departure_day_of_year.timetuple().tm_yday #converts dates to the number of the day in the year
+
+    if 'N' in (availability[arrival_day_of_year:departure_day_of_year]):
+        items['availability']='Not Available'
+    else:
+        items['availability']='Available'
+    
     parsed=json.dumps(items, ensure_ascii=False).encode('utf-8')
 
     print(parsed.decode())
 
-    
 
 get_basic_info()
+
+
